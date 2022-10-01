77aca87f00180c9631a745e00d470a5e7edd915f
Matias Ventura <matias.r.ventura@gmail.com>
2022-10-01 00:28:21 -0300

    SWR Update

--- ADAR-1/ADAR-1.ino
+++ ADAR-1/ADAR-1.ino
@@ -15,7 +15,10 @@
 #define PIN_ENC_A 2
 #define PIN_ENC_B 3
 #define PIN_ENC_BTN 4
-#define PIN_TONE_OUT 7
+#define PIN_TX_SENSE 5
+#define PIN_SWR_FWD A2
+#define PIN_SWR_REF A3
+
 
 #define FRAME_MS 33
 #define DEBOUNCE_DELAY_MS 50
@@ -31,12 +34,17 @@ const char LCD_BLANK_CHAR = ' ';
 int freqCal = 0;
 int Smin = 0;
 int Smax = 1023;
+int TxFwdMin = 0;
+int TxFwdMax = 1023;
+int TxRefMin = 0;
+int TxRefMax = 1023;
 
 AD9833 vfo(PIN_VFO_FNC); // Defaults to 25MHz internal reference frequency
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
 Encoder enc(PIN_ENC_A, PIN_ENC_B);
 
 float currFreq = START_FREQ;
+double currDispFreq = 0;
 long currMultiplier = 10;
 
 void setup() {
@@ -47,6 +55,7 @@ void setup() {
     setupLCDMain();
     enc.write(0);
     pinMode(PIN_ENC_BTN, INPUT_PULLUP);
+    pinMode(PIN_TX_SENSE, INPUT);
 }
 
 void setupVFO() {
@@ -67,27 +76,39 @@ unsigned long tLastFrame = 0;
 unsigned long tEncBtn = 0;
 bool encLow = false;
 bool encSP = false;
-//bool encLP = false;
+bool encLP = false;
 bool menu = false;
+bool tx = false;
 
 void loop() {
     if ((millis() - tLastFrame) > FRAME_MS) {
         tLastFrame = millis();
-        if (menu) frameMenu();
+        if (tx) frameTx;
+        else if (menu) frameMenu();
         else frameMain();
     }
 
     uint8_t encBtnState = digitalRead(PIN_ENC_BTN);
+
 
     if (encBtnState == LOW && encLow == false) {
         tEncBtn = millis();
         encLow = true;
     }
 
-    if (encLow && encBtnState == HIGH) {
-        if ((millis() - tEncBtn) > DEBOUNCE_DELAY_MS) {
-            if ((millis() - tEncBtn) > LONG_PRESS_MS) {
-                //encLP = true;
+    if (encLow) {
+        if (encBtnState == HIGH) {
+            if (encLP) {
+                encLow = false;
+                encLP = false;
+            }
+            else if ((millis() - tEncBtn) > DEBOUNCE_DELAY_MS) {
+                encSP = true;
+                encLow = false;
+            }
+        }
+        else {
+            if (encLP == false && (millis() - tEncBtn) > LONG_PRESS_MS) {
                 menu = !menu;
                 if (menu) {
                     menuDisplayItem();
@@ -96,12 +117,25 @@ void loop() {
                     storeSettings();
                     setupLCDMain();
                 }
-            }
-            else {
-                encSP = true;
+                encLP = true;
             }
         }
-        encLow = false;
+    }
+
+    uint8_t txState = digitalRead(PIN_TX_SENSE);
+
+    if (txState == HIGH && tx == false) {
+        SetupLcdTx();
+        tx = true;
+    }
+    else if (txState == LOW && tx == true) {
+        if (menu) {
+            menuDisplayItem();
+        }
+        else {
+            setupLCDMain();
+        }
+        tx = false;
     }
 
     delay(5);
@@ -122,14 +156,23 @@ void writeIntToEEPROM(int address, int number)
 
 void loadSettings() {
     freqCal = readIntFromEEPROM(0);
+    currDispFreq = (currFreq + BFO_FREQ + freqCal) / 1000.0;
     Smin = readIntFromEEPROM(2);
     Smax = readIntFromEEPROM(4);
+    TxFwdMin = readIntFromEEPROM(6);
+    TxFwdMax = readIntFromEEPROM(8);
+    TxRefMin = readIntFromEEPROM(10);
+    TxRefMax = readIntFromEEPROM(12);
 }
 
 void storeSettings() {
-    writeIntToEEPROM(0, freqCal);
-    writeIntToEEPROM(2, Smin);
-    writeIntToEEPROM(4, Smax);
+    if(freqCal != readIntFromEEPROM(0)) writeIntToEEPROM(0, freqCal);
+    if(Smin != readIntFromEEPROM(2)) writeIntToEEPROM(2, Smin);
+    if(Smax != readIntFromEEPROM(4)) writeIntToEEPROM(4, Smax);
+    if(TxFwdMin != readIntFromEEPROM(6)) writeIntToEEPROM(6, TxFwdMin);
+    if(TxFwdMax != readIntFromEEPROM(8)) writeIntToEEPROM(8, TxFwdMax);
+    if(TxRefMin != readIntFromEEPROM(10)) writeIntToEEPROM(10, TxRefMin);
+    if(TxRefMax != readIntFromEEPROM(12)) writeIntToEEPROM(12, TxRefMax);
 }
 
 //// ---------- ENCODER ----------
@@ -151,7 +194,7 @@ bool readEncSP() {
 }*/
 
 long encPos = 0;
-byte encCalCounter = 0;
+uint8_t encCalCounter = 0;
 int readEncoder() {
     int diff;
     long newEncPos = enc.read();
@@ -181,19 +224,18 @@ int readEncoder() {
     return diff;
 }
 
-float khz(float freq) {
+/*float khz(float freq) {
     return freq * 1000;
-}
+}*/
 
 
 //// ---------- MAIN SCREEN ----------
 
 void setupLCDMain() {
     lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print('[');
-    lcd.setCursor(10, 0);
-    lcd.print("] S0");
+    drawSignalBox(0);
+    lcd.setCursor(12, 0);
+    lcd.print("S0");
     lcd.setCursor(11, 1);
     lcd.print("kHz");
     refreshLCDFreq();
@@ -242,36 +284,28 @@ void printMultiplier() {
 void setFreq(float freq) {
     if (freq <= 0) return;
     if (freq == currFreq) return;
+
+    double newDispFreq = (freq + BFO_FREQ + freqCal) / 1000.0;
+    if (newDispFreq < 7005.0 || newDispFreq > 7298.0) return;
+
     currFreq = freq;
     vfo.SetFrequency(REG0, freq);
+    currDispFreq = newDispFreq;
     refreshLCDFreq();
 }
 
 void refreshLCDFreq() {
     char buff[9];
     lcd.setCursor(2, 1);
-    dtostrf((currFreq + BFO_FREQ + freqCal) / 1000, 8, 3, buff);
+    dtostrf(currDispFreq, 8, 3, buff);
     lcd.print(buff);
 }
 
-int lastSValue = 0;
+uint8_t lastSValue = 0;
 void refreshSMeter() {
-    int S = constrain(map(analogRead(PIN_SMETER), Smin, Smax, 0, 9), 0, 9);
+    uint8_t S = constrain(map(analogRead(PIN_SMETER), Smin, Smax, 0, 9), 0, 9);
     if (S != lastSValue) {
-        char buff[10];
-        if (S > lastSValue) {
-            lcd.setCursor(lastSValue + 1, 0);
-            for (int i = 0; i < S - lastSValue; i++) buff[i] = LCD_BLOCK_CHAR;
-            buff[S - lastSValue] = '\0';
-            lcd.print(buff);
-        }
-        else {
-            lcd.setCursor(S + 1, 0);
-            for (int i = 0; i < lastSValue - S; i++) buff[i] = LCD_BLANK_CHAR;
-            buff[lastSValue - S] = '\0';
-            lcd.print(buff);
-        }
-
+        drawSignalBar(S, lastSValue, 0);
         lcd.setCursor(13, 0);
         lcd.print(S);
         lastSValue = S;
@@ -335,7 +369,7 @@ void menuDisplayItem() {
     {
     case 0:
         menuDrawTop("Freq Cal");
-        menuDrawBottom((currFreq + BFO_FREQ + freqCal) / 1000, "kHz", 3);
+        menuDrawBottom(currDispFreq, "kHz", 3);
         break;
     case 1:
         menuDrawTop("S Min");
@@ -344,6 +378,22 @@ void menuDisplayItem() {
     case 2:
         menuDrawTop("S Max");
         menuDrawBottom(Smax, "");
+        break;
+    case 3:
+        menuDrawTop("SWR Fwd Min");
+        menuDrawBottom(TxFwdMin, "");
+        break;
+    case 4:
+        menuDrawTop("SWR Fwd Max");
+        menuDrawBottom(TxFwdMax, "");
+        break;
+    case 5:
+        menuDrawTop("SWR Ref Min");
+        menuDrawBottom(TxRefMin, "");
+        break;
+    case 6:
+        menuDrawTop("SWR Ref Max");
+        menuDrawBottom(TxRefMax, "");
         break;
     default:
         break;
@@ -355,7 +405,8 @@ void menuSetItem(double val) {
     {
     case 0:
         freqCal += val;
-        menuDrawValue((currFreq + BFO_FREQ + freqCal) / 1000, 3);
+        currDispFreq = (currFreq + BFO_FREQ + freqCal) / 1000.0;
+        menuDrawValue(currDispFreq, 3);
         break;
     case 1:
         Smin += val;
@@ -365,7 +416,90 @@ void menuSetItem(double val) {
         Smax += val;
         menuDrawValue(Smax);
         break;
+    case 3:
+        TxFwdMin += val;
+        menuDrawValue(TxFwdMin);
+        break;
+    case 4:
+        TxFwdMax += val;
+        menuDrawValue(TxFwdMax);
+        break;
+    case 5:
+        TxRefMin += val;
+        menuDrawValue(TxRefMin);
+        break;
+    case 6:
+        TxRefMax += val;
+        menuDrawValue(TxRefMax);
+        break;
     default:
         break;
+    }
+}
+
+//// ---------- TX SCREEN ----------
+
+void SetupLcdTx() {
+    lcd.clear();
+    drawSignalBox(0);
+    drawSignalBox(1);
+    lcd.setCursor(11, 0);
+    lcd.print('F');
+    lcd.setCursor(15, 0);
+    lcd.print('W');
+    lcd.setCursor(11, 1);
+    lcd.print('R');
+}
+
+uint8_t lastFwd = 0;
+uint8_t lastRef = 0;
+char txBuff[4];
+
+void frameTx() {
+    uint8_t fwd = constrain(map(analogRead(PIN_SWR_FWD), TxFwdMin, TxFwdMax, 0, 30), 0, 30);
+    if (fwd != lastFwd) {
+        drawSignalBar(fwd / 3, lastFwd / 3, 0);
+        lcd.setCursor(13, 0);
+        dtostrf(fwd / 2.0, 2, 0, txBuff);
+        lcd.print(txBuff);
+        lastFwd = fwd;
+    }
+    
+
+    uint8_t ref = constrain(map(analogRead(PIN_SWR_REF), TxFwdMin, TxFwdMax, 0, 30), 0, 30);
+    if (ref != lastRef) {
+        drawSignalBar(ref / 3, lastRef / 3, 1);
+        lcd.setCursor(13, 0);
+        dtostrf(ref / 10.0, 3, 1, txBuff);
+        lcd.print(txBuff);
+        lastRef = ref;
+    }
+}
+
+
+//// ---------- MISC ----------
+
+inline void drawSignalBox(int row) {
+    lcd.setCursor(0, row);
+    lcd.print('[');
+    lcd.setCursor(10, row);
+    lcd.print(']');
+}
+
+void drawSignalBar(int value, int lastValue, int row) {
+    if (value != lastValue) {
+        char buff[10];
+        if (value > lastValue) {
+            lcd.setCursor(lastValue + 1, row);
+            for (int i = 0; i < value - lastValue; i++) buff[i] = LCD_BLOCK_CHAR;
+            buff[value - lastValue] = '\0';
+            lcd.print(buff);
+        }
+        else {
+            lcd.setCursor(value + 1, row);
+            for (int i = 0; i < lastValue - value; i++) buff[i] = LCD_BLANK_CHAR;
+            buff[lastValue - value] = '\0';
+            lcd.print(buff);
+        }
     }
 }
